65aeb9ce22acb4828388fe67f80d87a72e902eb1
huyqng68 <huyqng68@gmail.com>
2025-01-15 15:31:34 +0700

    init project

---
+++ src/main/java/org/example/clone_whatsapp/CloneWhatsappApplication.java
@@ -0,0 +1,13 @@
+package org.example.clone_whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneWhatsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneWhatsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/clone_whatsapp/CloneWhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clone_whatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneWhatsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
